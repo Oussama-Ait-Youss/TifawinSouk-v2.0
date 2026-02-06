125e85a20c6f9ceb35c7fe4e54d55e671539d84d
Oussama-Ait-Youss <alikhay815@gmail.com>
2026-02-06 11:38:03 +0100

    TVS-67 #DONE feat: add fournisseur controller that contains the crud, and the define routes in the web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\FournisseurController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -12,9 +13,16 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    // authentification
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // fournisseur
+    Route::get('/fournisseur',[FournisseurController::class,'index']);
+    Route::get('/fournisseur/edit',[FournisseurController::class,'edit'])->name('fournisseur.edit');
+    Route::get('/fournisseur/create',[FournisseurController::class,'create'])->name('fournisseur.create');
+    Route::post('/fournisseur/{fournisseur}/update',[FournisseurController::class],'update')->name('fournisseur.update');
 });
 
 require __DIR__.'/auth.php';
